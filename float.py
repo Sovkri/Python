c8d1ec2df0021f4e4f54f9f5065d715ad468ce6c
Sovkri <ilya.nikolayev.2015@list.ru>
2024-08-29 14:51:51 +0300

    Add files via upload

---
+++ float.py
@@ -0,0 +1,7 @@
+a = float(input("ведите цифру:"))
+b = float(input("ведите цифру:"))
+
+print("A + B =",a + b )
+print("A - B =",a - b )
+print("A * B =",a * b )
+print("A / B =",a / b )
---
+++ input.py
@@ -0,0 +1,7 @@
+a = int(input("Выдитен цифру 1:"))
+b = int(input("ведите цифру 2 :"))
+
+print("A + B = ",a + b )
+print("A - B = ",a - b )
+print("A * B = ",a * b )
+print("A / B = ",a / b )
